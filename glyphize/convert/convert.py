fba36ddb104c96a75e948b53903126e96e9b54c3
Chris McKeel <chrisfox9158@gmail.com>
2026-08-06 03:06:37 -0400

    Add convert_to_map() for numpy-based custom-dtype cell map conversion

--- glyphize/convert/convert.py
+++ glyphize/convert/convert.py
@@ -1,4 +1,8 @@
 from PIL import Image
+import math
+import numpy as np
+
+from ..models import CellMap
 
 def load(image):
     with Image.open(image) as im:
@@ -39,3 +43,31 @@ def luminance(mean_rgb):
     weighted_sum = (0.299 * r) + (0.587 * g) + (0.114 * b) # based on ITU-R BT.601 standard
     normal_lum = weighted_sum / 255.0
     return normal_lum
+
+def convert_to_map(image, cell_size):
+    grid_width = math.ceil(image.width / cell_size)
+    grid_height = math.ceil(image.height / cell_size)
+
+    cell_dtype = np.dtype([
+        ("mean_rgb", np.uint8, (3,)),
+        ("brightness", np.float32)
+        ])
+
+    grid = np.empty((grid_height, grid_width), dtype=cell_dtype)
+
+    for row in range(grid_height):
+        for col in range(grid_width):
+            x0, y0 = col * cell_size, row * cell_size
+            x1 = min(x0 + cell_size, image.width)
+            y1 = min(y0 + cell_size, image.height)
+
+            pixel_coords = x0, y0, x1, y1
+            pixel_block = image.crop(pixel_coords)
+
+            rgb = average_color(pixel_block)
+            lum = luminance(rgb)
+
+            grid[row, col] = (rgb, lum)
+
+    return CellMap(grid, grid_width, grid_height, cell_size, image.width, image.height)
+
