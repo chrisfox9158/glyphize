2b69e2706a503ef4566452af43c74df706b07945
Chris McKeel <chrisfox9158@gmail.com>
2026-08-06 10:36:15 -0400

    Add fonts registry

---
+++ assets/fonts/fonts.py
@@ -0,0 +1,4 @@
+FONTS = {
+    "Intel-Regular": {"path": "assets/fonts/IntelOneMono-Regular.ttf", "scale": 1.0},
+    "Intel-Italic": {"path": "assets/fonts/IntelOneMono-Italic.ttf", "scale": 1.0},
+}
