62a0c5a0a01a2aac5a9e599ce625830e6d1ab8d5
Chris McKeel <chrisfox9158@gmail.com>
2026-08-06 11:10:12 -0400

    Test probabilistic rendering against high-contrast and midtone-dominant assets

--- tests/test_simple_render.py
+++ tests/test_simple_render.py
@@ -3,15 +3,20 @@ from glyphize.render.render import render_ascii
 from glyphize.convert.convert import convert
 from assets.fonts.fonts import FONTS
 
-def simple_render(cell_map, output_path=None):
-    rendering = render_ascii(cell_map, " .-=*#%", 0.0,
-                            FONTS["Intel-Regular"]["path"], FONTS["Intel-Regular"]["scale"],
-                            "black", "white")
+def simple_render(cell_map, output_path=None, curve_power=1.0):
+    rendering = render_ascii(cell_map=cell_map, ramp=" .-=*#%",
+                            floor_brightness=0.0, curve_power=curve_power,
+                            font_path=FONTS["Intel-Regular"]["path"], font_size_scale=FONTS["Intel-Regular"]["scale"],
+                            bg_color="black", glyph_color="white")
 
     if output_path:
         rendering.save(output_path)
     else:
         rendering.show()
 
-cell_map = convert("assets/skeleton.jpg", cell_size=8)
-simple_render(cell_map, "assets/skeleton_ascii_render.png")
+cell_map_skeleton = convert("assets/skeleton.jpg", cell_size=8)
+simple_render(cell_map_skeleton, "assets/skeleton_ascii_render_curved.png", 0.2)
+
+cell_map_wave = convert("assets/great_wave.jpg", cell_size=8)
+simple_render(cell_map_wave, "assets/great_wave_ascii_render_curved.png", 1.0)
+
